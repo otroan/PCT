e6be64649587f40fdeb5acf86acec79587f5cd38
Ole Troan <ot@cisco.com>
2022-05-20 15:21:38 +0200

    Initial version of openlayers map.
    Signed-off-by: Ole Troan <ot@cisco.com>

--- map/main.js
+++ map/main.js
@@ -1,17 +1,46 @@
 import './style.css';
 import {Map, View} from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
+import XYZ from 'ol/source/XYZ';
+import * as olProj from 'ol/proj';
+//import GeoJSON from 'ol/format/GeoJSON';
+import GPX from 'ol/format/GPX';
+import {OSM, Vector as VectorSource} from 'ol/source';
+import Feature from 'ol/Feature';
+import KML from 'ol/format/KML';
+import Overlay from 'ol/Overlay'
 
+import * as olCoordinate from 'ol/coordinate';
+
+const pct_vector = new VectorLayer({
+    source: new VectorSource({
+        url: 'data/pct.gpx',
+        format: new GPX(),
+    }),
+});
+
+const ole_vector = new VectorLayer({
+    source: new VectorSource({
+        url: 'data/OleTroan.kml',
+        // url: 'https://share.garmin.com/Feed/Share/OleTroan',
+        // url: 'https://share.garmin.com/Feed/Share/OleTroan\?d1\=2019-01-01',
+        format: new KML(),
+        crossOrigin: 'anonymous'
+    }),
+});
+      
 const map = new Map({
-  target: 'map',
-  layers: [
-    new TileLayer({
-      source: new OSM()
-    })
-  ],
-  view: new View({
-    center: [0, 0],
-    zoom: 2
-  })
+    target: 'map',
+    layers: [
+        // OpenStreetMap layer
+        new TileLayer({
+            source: new OSM()
+        }),
+        pct_vector,
+        ole_vector
+    ],
+    view: new View({
+        center: olProj.fromLonLat([-121.385723, 40.754215]),
+        zoom: 5
+    }),
 });
